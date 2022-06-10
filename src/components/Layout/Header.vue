2ae8e03ecaa4f3e59f4bbe57e62fd1e7c6c02413
hwyang <yanghw0911@gmail.com>
2022-06-10 17:32:15 +0900

    layout sample commit

---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="header">
+    <div class="logo_area">logo</div>
+    <div class="menu_area">menu</div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+.header {
+  margin: 10px auto;
+}
+.logo_area {
+  float: left;
+  box-sizing: border-box;
+}
+.menu_area {
+  float: left;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/views/Layout/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <Header></Header>
+  <router-view></router-view>
+  <Footer></Footer>
+</template>
+
+<script>
+import Header from '@/components/Layout/Header';
+import Footer from '@/components/Layout/Footer';
+export default {
+  name: 'Layout',
+  components: { Header, Footer },
+};
+</script>
+
+<style scoped></style>
