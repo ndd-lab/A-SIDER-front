d0d140787a5f9263f149ee74869329838d203195
hwyang <yanghw0911@gmail.com>
2022-07-01 16:23:36 +0900

    프로젝트 공고 탭 레이아웃 제작중

--- src/components/Project/RecruitCreate.vue
+++ src/components/Project/RecruitCreate.vue
@@ -1,11 +1,58 @@
 <template>
-  <div class="mt-5">프로젝트 공고 생성</div>
+  <div class="mt-5">
+    <div class="project-item full">
+      <span class="item-title">프로젝트 모집 타이틀</span><br />
+      <b-input placeholder="ex) 열심히 일 할 노예를 구해요" />
+    </div>
+    <div class="project-item">
+      <span class="item-title">모집 상태</span>
+      <span class="item">
+        <b-select v-model="selectedStatus" class="mb-3">
+          <b-select-option value="C">옵션1</b-select-option>
+          <b-select-option value="D">옵션2</b-select-option>
+        </b-select>
+      </span>
+    </div>
+    <div v-for="n in recruitFieldCnt" :key="n + 'field'">
+      <RecruitField></RecruitField>
+    </div>
+    <button @click="recruitFieldCnt++">모집 대상 추가</button>
+  </div>
 </template>
 
 <script>
+import RecruitField from '@/components/Project/RecruitField';
+
 export default {
   name: 'RecruitCreate',
+  components: { RecruitField },
+  data() {
+    return {
+      selectedStatus: '',
+      recruitFieldCnt: 1,
+    };
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.project-item {
+  display: flex;
+  margin: 20px 20px;
+}
+.project-item .item-title {
+  width: 140px;
+}
+.project-item .item {
+  height: 35px;
+}
+.full {
+  flex-direction: column;
+}
+.full.item-title {
+  width: 100%;
+}
+.project-item span {
+  display: inline-block;
+}
+</style>
---
+++ src/components/Project/RecruitField.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="recruit-field mb-3">
+    <div class="project-item">
+      <span class="item-title">모집 분야</span>
+      <span class="item">
+        <b-select v-model="selectedStatus" class="mb-3">
+          <b-select-option value="C">개발</b-select-option>
+          <b-select-option value="D">디자인</b-select-option>
+          <b-select-option value="D">기획</b-select-option>
+        </b-select>
+      </span>
+    </div>
+    <div class="project-item">
+      <span class="item-title">모집 인원</span>
+      <span class="item">
+        <b-select v-model="selectedStatus" class="mb-3">
+          <b-select-option value="C">1</b-select-option>
+          <b-select-option value="D">2</b-select-option>
+        </b-select>
+      </span>
+    </div>
+    <div class="project-item">
+      <span class="item-title">경력</span>
+      <span class="item">
+        <b-select v-model="selectedStatus" class="mb-3">
+          <b-select-option value="C">신입</b-select-option>
+          <b-select-option value="D">2~4년차</b-select-option>
+          <b-select-option value="D">5년이상</b-select-option>
+        </b-select>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecruitField',
+  data() {
+    return { selectedStatus: '' };
+  },
+};
+</script>
+
+<style scoped>
+.recruit-field {
+  border: 1px solid rgba(0, 0, 0, 0.125);
+  box-shadow: 1px 1px 3px 1px #dadce0;
+  border-radius: 10px;
+}
+.project-item {
+  display: flex;
+  margin: 20px 20px;
+}
+.project-item .item-title {
+  width: 140px;
+}
+.project-item .item {
+  height: 35px;
+}
+.full {
+  flex-direction: column;
+}
+.project-item span {
+  display: inline-block;
+}
+</style>
