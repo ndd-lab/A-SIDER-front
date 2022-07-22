3ece9c2cacbbf43faa1bbd947272896aeb687464
hwyang <yanghw0911@gmail.com>
2022-07-22 16:43:41 +0900

    dashboard 레이아웃 변경 및 slide컴포넌트 변경

--- src/components/Common/Slide.vue
+++ src/components/Common/Slide.vue
@@ -1,37 +1,37 @@
 <template>
-  <b-card>
-    <div class="title">
-      <h5>참여한 프로젝트</h5>
-      <router-link :to="{ name: '' }" class="secondary">
-        <b-icon variant="secondary" icon="plus"></b-icon>더보기</router-link
-      >
-    </div>
-    <Flicking
-      :options="{
-        circular: true,
-        stopOnHover: true,
-      }"
-      :plugins="plugin"
-    >
-      <div v-for="idx in list" class="flicking-panel" :key="idx">
-        <div class="card m-3" style="width: 18rem">
-          <img
-            src="@/assets/images/project-tools.jpg"
-            class="card-img-top"
-            alt=""
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title {{ idx }}</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a class="btn btn-primary">Go somewhere</a>
-          </div>
+  <!--  <b-card>-->
+  <!--    <div class="title">-->
+  <!--      <h5>참여한 프로젝트</h5>-->
+  <!--      <router-link :to="{ name: '' }" class="secondary">-->
+  <!--        <b-icon variant="secondary" icon="plus"></b-icon>더보기</router-link-->
+  <!--      >-->
+  <!--    </div>-->
+  <Flicking
+    :options="{
+      circular: true,
+      stopOnHover: true,
+    }"
+    :plugins="plugin"
+  >
+    <div v-for="idx in list" class="flicking-panel" :key="idx">
+      <div class="card m-3" style="width: 18rem">
+        <img
+          src="@/assets/images/project-tools.jpg"
+          class="card-img-top"
+          alt=""
+        />
+        <div class="card-body">
+          <h5 class="card-title">Card title {{ idx }}</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
-    </Flicking>
-  </b-card>
+    </div>
+  </Flicking>
+  <!--  </b-card>-->
 </template>
 
 <script>
--- src/views/MyPage/Dashboard.vue
+++ src/views/MyPage/Dashboard.vue
@@ -4,61 +4,46 @@
       <b-col cols="12">
         <b-card>
           <div class="title">
-            <h5>참여한 프로젝트</h5>
+            <h5>프로필</h5>
             <router-link :to="{ name: '' }" class="secondary">
               <b-icon variant="secondary" icon="plus"></b-icon
               >더보기</router-link
             >
           </div>
-          <ul>
-            <li>
-              <router-link
-                :to="{
-                  name: 'myPage-project-detail',
-                  params: { seq: 1 },
-                }"
-                >프로젝트1</router-link
-              >
-            </li>
-            <li>프로젝트2</li>
-            <li>프로젝트3</li>
-          </ul>
+          프로필 내용 영역
         </b-card>
       </b-col>
-      <b-col cols="6">
+      <b-col cols="12">
         <b-card>
           <div class="title">
-            <h5>참여한 프로젝트</h5>
+            <h5>평가</h5>
             <router-link :to="{ name: '' }" class="secondary">
               <b-icon variant="secondary" icon="plus"></b-icon
               >더보기</router-link
             >
           </div>
-          <ul>
-            <li>프로젝트1</li>
-            <li>프로젝트2</li>
-            <li>프로젝트3</li>
-          </ul>
-        </b-card>
-      </b-col>
-      <b-col cols="6">
-        <b-card>
-          <div class="title">
-            <h5>참여한 스터디 모임</h5>
-            <router-link :to="{ name: '' }" class="secondary">
-              <b-icon variant="secondary" icon="plus"></b-icon
-              >더보기</router-link
-            >
-          </div>
-          <ul>
-            <li>모임1</li>
-            <li>모임2</li>
-            <li>모임3</li>
-          </ul>
+          방사형 그래프 영역
         </b-card>
       </b-col>
     </b-row>
-    <div><Slide></Slide></div>
+    <b-card>
+      <div class="title">
+        <h5>참여한 프로젝트</h5>
+        <router-link :to="{ name: '' }" class="secondary">
+          <b-icon variant="secondary" icon="plus"></b-icon>더보기</router-link
+        >
+      </div>
+      <Slide></Slide>
+    </b-card>
+    <b-card>
+      <div class="title">
+        <h5>스터디 모임</h5>
+        <router-link :to="{ name: '' }" class="secondary">
+          <b-icon variant="secondary" icon="plus"></b-icon>더보기</router-link
+        >
+      </div>
+      <Slide></Slide>
+    </b-card>
   </div>
 </template>
 
